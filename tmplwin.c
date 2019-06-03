171a284bfdc7ebec1292f8941152ee71b7d09538
alzoi <alzoi@ya.ru>
2019-06-03 21:14:00 +0500

    Update tmplwin.c

--- tmplwin.c
+++ tmplwin.c
@@ -11,6 +11,8 @@
 #include <malloc.h>
 #include <memory.h>
 
+#include <crtdbg.h>
+
 LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
 
 int WINAPI _tWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR szCmdLine, int iCmdShow) {
@@ -25,7 +27,7 @@ int WINAPI _tWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR szCmdLin
 	WNDCLASSEX   wndclass = {0};
 
 	// Регистрация в ОС класса окна.
-	wndclass.cbSize         = sizeof(WNDCLASSEX);
+	wndclass.cbSize			= sizeof(WNDCLASSEX);
 	wndclass.hInstance		= hInstance;
 	wndclass.lpszClassName	= szAppName;
 	wndclass.lpfnWndProc		= WndProc;
@@ -33,9 +35,9 @@ int WINAPI _tWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR szCmdLin
 	wndclass.cbClsExtra		= 0;
 	wndclass.cbWndExtra		= 0;
 	wndclass.hIcon				= LoadIcon(nullptr, IDI_APPLICATION);
-	wndclass.hIconSm        = LoadIcon(nullptr, IDI_APPLICATION);
+	wndclass.hIconSm			= LoadIcon(nullptr, IDI_APPLICATION);
 	wndclass.hCursor			= LoadCursor(nullptr, IDC_ARROW);
-	wndclass.hbrBackground  = nullptr; //(HBRUSH)GetStockObject(WHITE_BRUSH);
+	wndclass.hbrBackground	= nullptr; //(HBRUSH)GetStockObject(WHITE_BRUSH);
 	wndclass.lpszMenuName	= nullptr;
 
 	if (!RegisterClassEx(&wndclass)) {
@@ -59,13 +61,40 @@ int WINAPI _tWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR szCmdLin
 		// Произвольный параметр создания окна.
 		nullptr
 	);              
-
+	// Открываем окно.
 	ShowWindow(hwnd, iCmdShow);
 	//UpdateWindow(hwnd);
 
-	while(GetMessage(&msg, nullptr, 0, 0)) {
-		TranslateMessage(&msg);
-		DispatchMessage(&msg);
+	//while(GetMessage(&msg, nullptr, 0, 0)){}
+	while(1) {
+		
+		// Получаем входящее (переданное ОС) сообщение из очереди сообщений нашей программы, если сообщения нет функция не блокирует поток.
+		if(PeekMessage(&msg, NULL, 0, 0, PM_REMOVE)) {
+			// Нажатие и отпускание клавиш, в переменную msg.wParam операционная система помещает скан-код нажатой клавиши клавиатуры.
+			if (msg.message == WM_KEYDOWN || msg.message == WM_SYSKEYDOWN || msg.message == WM_KEYUP || msg.message == WM_SYSKEYDOWN) {
+				_RPT3(0, "MES %x %c %x\n", msg.message, (TCHAR)msg.wParam, msg.wParam);
+			}			
+			
+			// Выход.
+			if (msg.message == WM_QUIT) {
+				_RPT1(0, "%s\n", "WM_QUIT");
+				break;
+			}
+
+			// Если произошло нажатие клавиши клавиатуры, просим ОС поместить в очередь входящих сообщений сообщение WM_CHAR,
+			// в переменную msg.wParam операционная система помещает код нажатой клавиши в кодировке Windows-1251.
+			if (msg.message == WM_KEYDOWN || msg.message == WM_SYSKEYDOWN || msg.message == WM_KEYUP || msg.message == WM_SYSKEYDOWN) {
+				TranslateMessage(&msg);
+			}
+
+			// Отправляем текущее, полученное функцией PeekMessage, сообщение в ОС, которая передаст его нашей оконной процедуре.
+			DispatchMessage(&msg);
+			
+		}
+		// Обработка, если в очереди нет сообщения.
+		else {
+
+		}
 	}
 	return msg.wParam;
 }
@@ -93,8 +122,15 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam) 
 			EndPaint(hwnd, &ps);
 			
 			return 0;
-
+		case WM_CHAR:
+			_RPT3(0, "%s %c %x\n", "WM_CHAR", (TCHAR)wParam, wParam);
+			return 0;
+		// Сообщение о закрытии окна.
 		case WM_DESTROY:
+			
+			_RPT1(0, "%s\n", "WM_DESTROY");
+			
+			// Отправляем сообщение WM_QUIT в оконную процедуру.
 			PostQuitMessage(0);
 			return 0;
 	}
